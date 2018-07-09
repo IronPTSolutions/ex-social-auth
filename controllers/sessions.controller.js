cf35cd8667f2af05f097d38894948b0388734c06
Carlos del Prado <carlos.delprado@11paths.com>
2018-07-09 21:36:39 +0200

    feat: login form

--- controllers/sessions.controller.js
+++ controllers/sessions.controller.js
@@ -1,8 +1,39 @@
+const passport = require('passport');
 
 module.exports.create = (req, res, next) => {
   res.render('sessions/create');
 }
 
 module.exports.doCreate = (req, res, next) => {
-  res.render('sessions/create');
+
+  function renderWithErrors(errors) {
+    res.status(400).render('sessions/create', {
+      user: req.body,
+      errors: errors
+    });
+  }
+
+  const { email, password } = req.body;
+  if (!email || !password) {
+    renderWithErrors({
+      email: email ? undefined : 'Email is required',
+      password: password ? undefined : 'Password is required'
+    });
+  } else {
+    passport.authenticate('local-auth', (error, user, validation) => {
+      if (error) {
+        next(error);
+      } else if (!user) {
+        renderWithErrors(validation);
+      } else {
+        req.login(user, (error) => {
+          if (error) {
+            next(error)
+          } else {
+            res.redirect(`/users/${user._id}`)
+          }
+        });
+      }
+    })(req, res, next);
+  }
 }
