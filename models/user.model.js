79cfcb02e9a206d2df22c94a22abbbcadfb60ad0
Carlos del Prado <carlos.delprado@11paths.com>
2018-07-09 20:23:28 +0200

    feat: user model

---
+++ models/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'Name is required'
+  },
+  email: {
+    type: String,
+    required: 'Email is required',
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: 'Password is required',
+  }
+}, { timestamps: true });
+
+userSchema.pre('save', (next) => {
+  if (this.isModified('password')) {
+    bcrypt.genSalt(SALT_WORK_FACTOR)
+      .then(salt => {
+        return bcrypt.hash(this.password, salt)
+      })
+      .then(hash => {
+        this.password = hash;
+        next();
+      })
+      .catch(error => next(error));
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.checkPassword = function(password) {
+  return bcrypt.compare(password, this.password);
+}
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
